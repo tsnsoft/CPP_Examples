ab8e6bd0f8c96d7284ed5714e7fb9dd3961ec444
proffix4 <proffix4@gmail.com>
2020-02-13 18:50:58 +0600

    update

--- Lab_0/main.cpp
+++ Lab_0/main.cpp
@@ -4,21 +4,23 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    int number;
+    int number1;
+    float number2;
 
     /* Стиль С из  <stdio.h> */
-    printf("Введите число: ");
-    scanf("\n%d", &number);
-    printf("mumber = %d\n", number);
+    printf("Введите целое и вещественное число через пробел: ");
+    scanf("\n%d %f", &number1, &number2);
+    printf("number1 = %d, number2 = %f \n", number1, number2);
     puts("Спасибо!");
         
     /* Стиль С++ из  <iostream> */
     cout << "Введите еще число: ";
-    cin >> number;
-    cout << "Вы ввели число " << number << endl;
+    cin >> number1;
+    cout << "Вы ввели число " << number1 << endl;
 
     /* Создание задержки на экране. Стиль С++ из  <iostream> */
     cout << "\nНажмите Enter для завершения программы...\n";
+    // Очистим ввод с клавиатуры и будем ожидать нажатие клавиши "Enter" 
     cin.ignore(); cin.get();
 
     return 0;
