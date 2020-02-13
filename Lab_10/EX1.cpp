796fb759dfdd4edce88d81d13dd7bf866f16a63c
proffix4 <proffix4@gmail.com>
2020-02-13 11:05:35 +0600

    update

--- Lab_10/EX1.cpp
+++ Lab_10/EX1.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
- 
+
 struct DateStruct {
     int day;
     int month;
     int year;
 };
- 
+
 void print(DateStruct &date) {
-    std::cout << date.day<< "/" << date.month << "/" << date.year;
+    std::cout << date.day << "/" << date.month << "/" << date.year;
 }
- 
+
 int EX1() {
-    DateStruct today { 12, 11, 2018}; // используем uniform инициализацию
- 
+    DateStruct today{ 12, 11, 2018}; // используем uniform инициализацию
     today.day = 18; // используем оператор выбора члена для выбора члена структуры
     print(today); // 18/11/2018
- 
     return 0;
 }
--- Lab_10/EX6.cpp
+++ Lab_10/EX6.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
- 
-class Fraction
-{
+
+class Fraction {
 private:
     int m_numerator;
     int m_denominator;
- 
+
 public:
     Fraction() // конструктор по умолчанию 
     {
-         m_numerator = 0;
-         m_denominator = 1;
+        m_numerator = 0;
+        m_denominator = 1;
     }
- 
-    int getNumerator() { return m_numerator; }
-    int getDenominator() { return m_denominator; }
-    double getValue() { return static_cast<double>(m_numerator) / m_denominator; }
+
+    int getNumerator() {
+        return m_numerator;
+    }
+
+    int getDenominator() {
+        return m_denominator;
+    }
+
+    double getValue() {
+        return static_cast<double> (m_numerator) / m_denominator;
+    }
 };
- 
-int EX6()
-{
+
+int EX6() {
     Fraction drob; // так как нет никаких аргументов, то вызывается конструктор по умолчанию Fraction()
     std::cout << drob.getNumerator() << "/" << drob.getDenominator() << '\n';
- 
+
     return 0;
 }
